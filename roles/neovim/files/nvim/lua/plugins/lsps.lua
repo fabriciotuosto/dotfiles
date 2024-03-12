4bd43d64aae8fbcd0cf08b3f03ef5b4f9b530aff
Fabricio Tuosto <fabricio.tuosto@gmail.com>
2024-03-11 22:34:25 -0300

    Centralize LSp configuration to make better defaults, make nevim version a fact to change it later

--- roles/neovim/files/nvim/lua/plugins/lsps.lua
+++ roles/neovim/files/nvim/lua/plugins/lsps.lua
@@ -8,6 +8,11 @@ return {
     {
         "williamboman/mason-lspconfig.nvim",
         config = function()
+            local capabilities = vim.tbl_deep_extend(
+                'force',
+                vim.lsp.protocol.make_client_capabilities(),
+                require("cmp_nvim_lsp").default_capabilities()
+            )
             require("mason-lspconfig").setup({
                 ensure_installed = {
                     "rust_analyzer",
@@ -30,16 +35,96 @@ return {
                     "yamlls",
                     "zls",
                     "gopls",
-                }
+                },
+                handlers = {
+                    function(server_name)
+                        local opts = { capabilities = capabilities }
+                        require('lspconfig')[server_name].setup(opts)
+                    end,
+                    ["elixirls"] = function()
+                        local elixirls = require('mason-registry').get_package('elixir-ls')
+                        require('lspconfig').elixirls.setup({
+                            capabilities = capabilities,
+                            cmd = { elixirls:get_install_path() .. "/language_server.sh" },
+                            settings = {
+                                elixirLS = {
+                                    dialyzerEnabled = true,
+                                    fetchDeps = false,
+                                    enableTestLenses = false,
+                                    suggestSpecs = false,
+                                }
+                            }
+                        })
+                    end,
+                    ["gopls"] = function()
+                        local util = require("lspconfig/util")
+                        require('lspconfig').gopls.setup({
+                            capabilities = capabilities,
+                            cmd = { "gopls" },
+                            filetypes = { "go", "gomod", "gowork", "gotmpl", "tmpl" },
+                            root_dir = util.root_pattern("go.work", "go.mod"),
+                            settings = {
+                                gopls = {
+                                    staticcheck = true,
+                                    completeUnimported = true,
+                                    analyses = {
+                                        unusedparams = true,
+                                    }
+                                },
+                            },
+
+                        })
+                    end,
+                    ["pyright"] = function()
+                        require('lspconfig').pyright.setup({
+                            capabilities = capabilities,
+                            settings = {
+                                pyright = {
+                                    autoImportCompletion = true,
+                                },
+                                python = {
+                                    analysis = {
+                                        autoSearchPaths = true,
+                                        diagnosticMode = "openFilesOnly",
+                                        useLibraryCodeForTypes = true,
+                                        typeCheckingMode = "basic"
+                                    }
+                                }
+                            }
+                        })
+                    end,
+                    ["denols"] = function()
+                        require('lspconfig').denols.setup({
+                            capabilities = capabilities,
+                            root_dir = require('lspconfig').util.root_pattern("deno.json", "deno.jsonc")
+                        })
+                    end,
+                    ["tsserver"] = function()
+                        require('lspconfig').tsserver.setup({
+                            capabilities = capabilities,
+                            root_dir = require('lspconfig').util.root_pattern("package.json", "tsconfig.json",
+                                "jsconfig.json"),
+                            init_options = {
+                                preferences = {
+                                    disableSuggestions = true,
+                                }
+                            },
+                            commands = {
+                                OrganizeImports = {
+                                    function()
+                                        vim.lsp.buf.execute_command({
+                                            command = "_typescript.organizeImports",
+                                            arguments = { vim.api.nvim_buf_get_name(0) }
+                                        })
+                                    end,
+                                    description = "Organize Imports"
+                                }
+                            }
+                        })
+                    end
+                },
             })
         end,
-        -- handlers = {
-        --     function(server_name)
-        --         local server = servers[server_name] or {}
-        --         server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-        --         require('lspconfig')[server_name].setup(server)
-        --     end
-        -- }
     },
     {
         "neovim/nvim-lspconfig",
@@ -63,104 +148,6 @@ return {
                     vim.keymap.set({ "n", "x" }, "<leader>bf", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
                     vim.keymap.set({ "n", "x" }, "<leader>ca", vim.lsp.buf.code_action, opts)
                 end,
-            })
-            local lspconfig = require("lspconfig")
-            local capabilities = vim.lsp.protocol.make_client_capabilities()
-            capabilities = vim.tbl_deep_extend('force', capabilities, require("cmp_nvim_lsp").default_capabilities())
-
-            local opts = { capabilities = capabilities }
-            lspconfig.lua_ls.setup(opts)
-            lspconfig.clangd.setup(opts)
-            lspconfig.angularls.setup(opts)
-            lspconfig.dockerls.setup(opts)
-            lspconfig.html.setup(opts)
-            lspconfig.cssls.setup(opts)
-            lspconfig.emmet_ls.setup(opts)
-            lspconfig.jsonls.setup(opts)
-            lspconfig.sqlls.setup(opts)
-            lspconfig.rust_analyzer.setup(opts)
-            lspconfig.marksman.setup(opts)
-            lspconfig.terraformls.setup(opts)
-            lspconfig.templ.setup(opts)
-            lspconfig.yamlls.setup(opts)
-            lspconfig.zls.setup(opts)
-            lspconfig.docker_compose_language_service.setup(opts)
-
-            local util = require("lspconfig/util")
-            local mason_registry = require('mason-registry')
-            local elixirls = mason_registry.get_package('elixir-ls')
-            lspconfig.elixirls.setup({
-                capabilities = capabilities,
-                cmd = { elixirls:get_install_path() .. "/language_server.sh" },
-                settings = {
-                    elixirLS = {
-                        dialyzerEnabled = true,
-                        fetchDeps = false,
-                        enableTestLenses = false,
-                        suggestSpecs = false,
-                    }
-                }
-            })
-            lspconfig.gopls.setup({
-                capabilities = capabilities,
-                cmd = { "gopls" },
-                filetypes = { "go", "gomod", "gowork", "gotmpl", "tmpl" },
-                root_dir = util.root_pattern("go.work", "go.mod"),
-                settings = {
-                    gopls = {
-                        staticcheck = true,
-                        completeUnimported = true,
-                        analyses = {
-                            unusedparams = true,
-                        }
-                    },
-                },
-            })
-
-            lspconfig.denols.setup({
-                capabilities = capabilities,
-                root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc")
-            })
-
-            local function organize_imports()
-                local params = {
-                    command = "_typescript.organizeImports",
-                    arguments = { vim.api.nvim_buf_get_name(0) }
-                }
-                vim.lsp.buf.execute_command(params)
-            end
-
-            lspconfig.pyright.setup({
-                capabilities = capabilities,
-                settings = {
-                    pyright = {
-                        autoImportCompletion = true,
-                    },
-                    python = {
-                        analysis = {
-                            autoSearchPaths = true,
-                            diagnosticMode = "openFilesOnly",
-                            useLibraryCodeForTypes = true,
-                            typeCheckingMode = "basic"
-                        }
-                    }
-                }
-            })
-
-            lspconfig.tsserver.setup({
-                capabilities = capabilities,
-                root_dir = lspconfig.util.root_pattern("package.json", "tsconfig.json", "jsconfig.json"),
-                init_options = {
-                    preferences = {
-                        disableSuggestions = true,
-                    }
-                },
-                commands = {
-                    OrganizeImports = {
-                        organize_imports,
-                        description = "Organize Imports"
-                    }
-                }
             })
         end
     },
