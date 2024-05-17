67e6d0078098b3131842d568ff85bc07fe75c834
Fabricio Tuosto <fabricio.tuosto@gmail.com>
2024-05-17 03:31:55 -0300

    Added git mappings to neovim

--- roles/neovim/files/nvim/lua/keymaps/telescope.lua
+++ roles/neovim/files/nvim/lua/keymaps/telescope.lua
@@ -7,6 +7,10 @@ vim.keymap.set("n", "<leader>ff",
     { desc = "[F]ind [F]iles in directory" }
 )
 vim.keymap.set("n", "<C-f>", builtin.git_files, { desc = "[F]ind [P]rojects files" })
+vim.keymap.set("n", "<leader>fgh", builtin.git_bcommits, { desc = "[F]ind [G]it buffer [H]istory" })
+vim.keymap.set("n", "<leader>fgc", builtin.git_commits, { desc = "[F]ind [G]it [C]ommits" })
+vim.keymap.set("n", "<leader>fgb", builtin.git_branches, { desc = "[F]ind [G]it [B]ranches" })
+vim.keymap.set("n", "<leader>fgs", builtin.git_status, { desc = "[F]ind [G]it [S]tatus" })
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "[F]ind [G]rep in project files" })
 vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "[F]ind [B]uffers" })
 vim.keymap.set("n", "<leader>fvc", builtin.commands, { desc = "[F]ind [V]im [C]ommands" })
